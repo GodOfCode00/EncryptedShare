1e021f1aaaa56b7487b3dc64067bbfe1dc8ba84e
Mousumi Tripathy <78490465+MUSU-CODE@users.noreply.github.com>
2021-10-13 19:35:41 +0530

    complete FileTableModel class

--- src/com/sbr/FileTableModel.java
+++ src/com/sbr/FileTableModel.java
@@ -8,19 +8,79 @@ public class FileTableModel extends AbstractTableModel {
     private File[] files;
     private FileSystemView fileSystemView=FileSystemView.getFileSystemView();
     private String[] columns= {"Icon","File","Path/name","Size","Last Modified","R","W","E","D","F"};
-
-    @Override
-    public int getRowCount() {
-        return 0;
+    FileTableModel() {
+        this(new File[0]);
     }
 
-    @Override
+    FileTableModel(File[] files) {
+        this.files = files;
+    }
+
+    public Object getValueAt(int row, int column) {
+        File file = files[row];
+        switch (column) {
+            case 0:
+                return fileSystemView.getSystemIcon(file);
+            case 1:
+                return fileSystemView.getSystemDisplayName(file);
+            case 2:
+                return file.getPath();
+            case 3:
+                return file.length();
+            case 4:
+                return file.lastModified();
+            case 5:
+                return file.canRead();
+            case 6:
+                return file.canWrite();
+            case 7:
+                return file.canExecute();
+            case 8:
+                return file.isDirectory();
+            case 9:
+                return file.isFile();
+            default:
+                System.err.println("Logic Error");
+        }
+        return "";
+    }
+
     public int getColumnCount() {
-        return 0;
+        return columns.length;
     }
 
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+    public Class<?> getColumnClass(int column) {
+        switch (column) {
+            case 0:
+                return ImageIcon.class;
+            case 3:
+                return Long.class;
+            case 4:
+                return Date.class;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                return Boolean.class;
+        }
+        return String.class;
+    }
+
+    public String getColumnName(int column) {
+        return columns[column];
+    }
+
+    public int getRowCount() {
+        return files.length;
+    }
+
+    public File getFile(int row) {
+        return files[row];
+    }
+
+    public void setFiles(File[] files) {
+        this.files = files;
+        fireTableDataChanged();
     }
 }
